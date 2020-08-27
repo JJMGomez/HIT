13378aa94d40eff8b364e1bf20adbb05e37455b6
jocelynnnn <465497055@qq.com>
2020-08-27 20:26:04 +0800

    20200827作业：咖啡店（装饰者模式）

--- src/CoffeeShop/CoffeShopTest.java
+++ src/CoffeeShop/CoffeShopTest.java
@@ -6,87 +6,106 @@ import javax.swing.*;
 import java.util.Scanner;
 
 public class CoffeShopTest {
+
+    public static final int COFFEETYPE = 3;
+    public static final int SIZETYPE = 3;
+    public static final int CARAMELTYPE = 3;
+
+    public static Scanner scanner = new Scanner(System.in);
+
+
+    public static Beverage ChooseCoffee(int coffeeIndex){
+        switch (coffeeIndex) {
+            case 1:
+                return new Espresso();
+            case 2:
+                return new Latte();
+            case 3:
+                return new Frappuccino();
+            default:
+                return null;
+        }
+    }
+
+    public static void ChooseCoffeeSize(Beverage beverage,int coffeeIndex){
+        switch (coffeeIndex) {
+            case 1:
+                beverage.setSize(Size.Tall);
+                break;
+            case 2:
+                beverage.setSize(Size.Grande);
+                break;
+            case 3:
+                beverage.setSize(Size.Venti);
+                break;
+        }
+    }
+
+    //验证输入：判断是否为数字，判断输入的数字范围是否正确
+    public static int InputCheck(int num){
+        int output;
+        if (scanner.hasNextInt()) {
+            output = Integer.parseInt(scanner.next());
+            if (output >= 0 && output<=num){
+                return output;
+            }
+        }
+        System.out.println("请输入正确数字！");
+        return -1;
+    }
+
     public static void main(String[] args) {
 
         double totalCost = 0.0;
         int totalNum = 0;
         boolean flag = true;
-        ok:
         while (flag) {
+
             System.out.println("—————————————————————————");
             System.out.println("欢迎点单～");
             System.out.println("请选择咖啡：");
             System.out.println("1. 浓缩    2. 拿铁   3.星冰乐  （输入其他内容退出点单，并计算总价） ");
-            Scanner scanner = new Scanner(System.in);
 
-            int coffeeIndex = 1;
-            if (scanner.hasNextInt()) {
-                coffeeIndex = Integer.parseInt(scanner.next());
-            } else {
-                System.out.println("请输入数字！");
+
+            int coffeeIndex = InputCheck(COFFEETYPE);
+            if (coffeeIndex==-1){
                 flag = false;
                 break;
             }
 
-            Beverage coffee;
-            switch (coffeeIndex) {
-                case 1:
-                    coffee = new Espresso();
-                    break;
-                case 2:
-                    coffee = new Latte();
-                    break;
-                case 3:
-                    coffee = new Frappuccino();
-                    break;
-                default:
-                    coffee = new Frappuccino();
-                    flag = false; //默认
-                    break ok;
+            Beverage coffee = ChooseCoffee(coffeeIndex);
+            if (coffee== null){
+                flag = false; //默认
+                break;
             }
+
 
             System.out.println("请选择杯型：");
             System.out.println("1. 中杯    2. 大杯   3.超大杯  （默认大杯）");
 
-            int size;
-            if (scanner.hasNextInt()) {
-                size = Integer.parseInt(scanner.next());
-            } else {
-                System.out.println("请输入数字！");
+            int size = InputCheck(SIZETYPE);
+            if (size==-1){
                 flag = false;
                 break;
             }
 
-            switch (size) {
-                case 1:
-                    coffee.setSize(Size.Tall);
-                    break;
-                case 2:
-                    coffee.setSize(Size.Grande);
-                    break;
-                case 3:
-                    coffee.setSize(Size.Venti);
-                    break;
-            }
+            ChooseCoffeeSize(coffee,size);
 
             Beverage coffeeWithCondiment = coffee;
 
-
-
             System.out.println("请选择加料：");
             System.out.println("1. 牛奶    2. 焦糖   3.不想加了   （可以加多次，默认不加）");
-            int condimentIndex;
+            int condimentIndex= InputCheck(CARAMELTYPE);
 
-            if (scanner.hasNextInt()) {
-                condimentIndex = Integer.parseInt(scanner.next());
-                if (condimentIndex == 1 || condimentIndex == 2 ){
+            if (condimentIndex!=-1) {
+
                     System.out.println("请输入加料份数：");
                     int condimentNum;
 
                     if (scanner.hasNextInt()) {
                         condimentNum = Integer.parseInt(scanner.next());
                     } else {
-                        System.out.println("请输入数字！");
+                        System.out.println("请输入正确数字！");
                         flag = false;
                         break;
                     }
@@ -106,7 +125,7 @@ public class CoffeShopTest {
                                 break ;
                         }
                     }
-                }
+
             } else {
                 System.out.println("请输入数字！");
                 flag = false;
@@ -116,12 +135,12 @@ public class CoffeShopTest {
 
 
 
-            System.out.println(coffeeWithCondiment.getSize() + coffeeWithCondiment.getDescription() + " 价格是：" + coffeeWithCondiment.cost());
+            System.out.println("您点了 " + coffeeWithCondiment.getSize() + coffeeWithCondiment.getDescription() + " 价格是：" + coffeeWithCondiment.cost());
             totalCost += coffeeWithCondiment.cost();
             totalNum ++;
         }
 
-        System.out.println("您点了 " + totalNum + " 杯，总价： " + totalCost );
+        System.out.println("您一共点了 " + totalNum + " 杯，总价： " + totalCost );
     }
 
 
